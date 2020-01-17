25c8977a8d3384e9ab44d0faf1e70dab1c3eb940
Charles <charlesdpulliam@gmail.com>
2020-01-17 15:16:35 -0600

    step 7

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 
 import store from './store';
+import api from './api';
 
-const generateItemElement = function (item) {
+const generateItemElement = function(item) {
   let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
   if (!item.checked) {
     itemTitle = `
@@ -26,12 +27,12 @@ const generateItemElement = function (item) {
     </li>`;
 };
 
-const generateShoppingItemsString = function (shoppingList) {
-  const items = shoppingList.map((item) => generateItemElement(item));
+const generateShoppingItemsString = function(shoppingList) {
+  const items = shoppingList.map(item => generateItemElement(item));
   return items.join('');
 };
 
-const render = function () {
+const render = function() {
   // Filter item list if store prop is true by item.checked === false
   let items = [...store.items];
   if (store.hideCheckedItems) {
@@ -45,23 +46,28 @@ const render = function () {
   $('.js-shopping-list').html(shoppingListItemsString);
 };
 
-const handleNewItemSubmit = function () {
+const handleNewItemSubmit = function() {
   $('#js-shopping-list-form').submit(function (event) {
     event.preventDefault();
     const newItemName = $('.js-shopping-list-entry').val();
     $('.js-shopping-list-entry').val('');
-    store.addItem(newItemName);
-    render();
+
+    api.createItem(newItemName)
+      .then(res => res.json())
+      .then(newItem => {
+        store.addItem(newItem);
+        render();
+      });
   });
 };
 
-const getItemIdFromElement = function (item) {
+const getItemIdFromElement = function(item) {
   return $(item)
     .closest('.js-item-element')
     .data('item-id');
 };
 
-const handleDeleteItemClicked = function () {
+const handleDeleteItemClicked = function() {
   // like in `handleItemCheckClicked`, we use event delegation
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
     // get the index of the item in store.items
@@ -73,17 +79,19 @@ const handleDeleteItemClicked = function () {
   });
 };
 
-const handleEditShoppingItemSubmit = function () {
+const handleEditShoppingItemSubmit = function() {
   $('.js-shopping-list').on('submit', '.js-edit-item', event => {
     event.preventDefault();
     const id = getItemIdFromElement(event.currentTarget);
-    const itemName = $(event.currentTarget).find('.shopping-item').val();
+    const itemName = $(event.currentTarget)
+      .find('.shopping-item')
+      .val();
     store.findAndUpdateName(id, itemName);
     render();
   });
 };
 
-const handleItemCheckClicked = function () {
+const handleItemCheckClicked = function() {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     const id = getItemIdFromElement(event.currentTarget);
     store.findAndToggleChecked(id);
@@ -91,14 +99,14 @@ const handleItemCheckClicked = function () {
   });
 };
 
-const handleToggleFilterClick = function () {
+const handleToggleFilterClick = function() {
   $('.js-filter-checked').click(() => {
     store.toggleCheckedFilter();
     render();
   });
 };
 
-const bindEventListeners = function () {
+const bindEventListeners = function() {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
